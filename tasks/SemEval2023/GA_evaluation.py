5e9102f527af6288fd0616c76b07d81757977200
araag2 <artur.guimas@gmail.com>
2023-12-31 00:15:54 +0000

    updated GA Evaluation

--- tasks/SemEval2023/GA_evaluation.py
+++ tasks/SemEval2023/GA_evaluation.py
@@ -7,10 +7,9 @@ import re
 import argparse
 
 from datetime import datetime
-from auto_gptq import exllama_set_max_input_length
 from tqdm import tqdm
 from sklearn.metrics import f1_score, precision_score, recall_score
-from transformers import GPTQConfig, LlamaTokenizer, LlamaForCausalLM, AutoTokenizer, AutoModelForCausalLM
+from transformers import AutoTokenizer, AutoModelForCausalLM
 
 
 def safe_open_w(path: str):
@@ -21,31 +20,24 @@ def extract_info_from_query(query : dict) -> dict:
     relevant_info = {}
     relevant_info["hypothesis"] = query["Statement"]
     relevant_info["primary_evidence"] = query["Primary_id_txt"]
-    if "Secondary_id_txt" in query:
-        relevant_info["secondary_evidence"] = query["Secondary_id_txt"]
+    relevant_info["secondary_evidence"] = query["Secondary_id_txt"] if "Secondary_id_txt" in query else ""
     return relevant_info
 
-def generate_query_from_prompt(text_to_replace: dict, prompt_dict: dict) -> str:
-    base_prompt = prompt_dict["task_template_prompt_comparison"] 
+def generate_query_from_prompt(text_to_replace: dict, prompt: str) -> str:
+    prompt = prompt.replace("$primary_evidence", text_to_replace["primary_evidence"])
+    prompt = prompt.replace("$secondary_evidence", text_to_replace["secondary_evidence"])
+    prompt = prompt.replace("$hypothesis", text_to_replace["hypothesis"])
+    return prompt
 
-    premise = prompt_dict["primary_premise"].replace("$primary_evidence", text_to_replace["primary_evidence"])
-    if "secondary_premise" in text_to_replace:
-        premise += prompt_dict["secondary_premise"].replace("$secondary_evidence", text_to_replace["secondary_evidence"])
-    options = prompt_dict["options"]
-    
-    # "$premise \n Question: Does this imply that $hypothesis? $options"
-    res = prompt_dict["prompt"].replace("$hypothesis", text_to_replace["hypothesis"])
+ENTAILMENT_LABELS = {"entailment", "yes", "y"}
+CONTRADICTION_LABELS = {"contradiction", "no", "not", "n"}
 
-    return res
-
-ENTAILMENT_LABELS = {"Entailment", "entailment", "Yes", "yes", "y"}
-CONTRADICTION_LABELS = {"Contradiction", "contradiction", "No", "no", "n"}
-
-def textlabel_2_binarylabel(text_label: str) -> int:
-    if text_label in ENTAILMENT_LABELS:
-        return 1
-    elif text_label in CONTRADICTION_LABELS:
-        return 0
+def textlabel_2_binarylabel(text_label: list[str]) -> int:
+    for label in text_label:
+        if label.lower() in ENTAILMENT_LABELS:
+            return 1
+        elif label.lower() in CONTRADICTION_LABELS:
+            return 0
     print(f'Text label: [{text_label=}.] This label executed a Random choice because the label was not found.')
     #return random.randint(0,1)
     return 1
@@ -61,32 +53,36 @@ def label_2_SemEval2023(labels : dict) -> dict:
         res[q_id] = {"Prediction" : pred}
     return res
 
-def create_qid_prompt_label_dict(queries : dict, qrels : dict, prompts : dict) -> dict:
+def create_qid_prompt_label_dict(queries : dict, qrels : dict, prompt : str) -> dict:
     queries_dict = {}
     for q_id in queries:
-        queries_dict[q_id] = {}
-        queries_dict[q_id]["text"] = generate_query_from_prompt(extract_info_from_query(queries[q_id]), prompts)
-        queries_dict[q_id]["gold_label"] = textlabel_2_binarylabel(qrels[q_id]["Label"].strip())
+        queries_dict[q_id] = { 
+            "text" : generate_query_from_prompt(extract_info_from_query(queries[q_id]), prompt), 
+            "gold_label" : textlabel_2_binarylabel(qrels[q_id]["Label"].strip())
+        }
     return queries_dict
 
 def query_inference(model : object, tokenizer : object, queries : dict) -> dict:
     res_labels = {}
     with torch.inference_mode():
+        i = 0
         for q_id in tqdm(queries):
-            input_ids = tokenizer(queries[q_id]["text"][:1300] + queries[q_id]["text"][-1300:], return_tensors="pt").input_ids.to("cuda")
+            input_ids = tokenizer(queries[q_id]["text"], return_tensors="pt").input_ids.to("cuda")
             #input_ids = tokenizer(queries[q_id]["text"], return_tensors="pt").input_ids.to("cuda")
-            outputs = model.generate(input_ids, max_new_tokens=100)
+            outputs = model.generate(input_ids, max_new_tokens=30, top_k = 5, do_sample=True)
             decoded_output = tokenizer.decode(outputs[0][input_ids[0].shape[0]:]).strip()
             decoded_output_sub = re.sub("[,!\.]+", " ", decoded_output)
             decoded_output_sub = re.sub("(\\n)+", " ", decoded_output_sub)
             decoded_output_sub = re.sub("(<\/s>)+", " ", decoded_output_sub)
-            print(f'The postprocessed decoded output was {decoded_output_sub.split(" ")=}')
-            res_labels[q_id] = textlabel_2_binarylabel(decoded_output_sub.split(" ")[0])
+            print(f'The postprocessed decoded output was {decoded_output_sub.split(" ")[:10]=}')
+            res_labels[q_id] = textlabel_2_binarylabel(decoded_output_sub.split(" ")[:10])
+            i += 1
+            if i > 10:
+                break
     return res_labels
 
 def single_query_inference(model : object, tokenizer : object, prompt : str) -> str:
     with torch.inference_mode():
-        print(f'{prompt=}')
         input_ids = tokenizer(prompt, return_tensors="pt").input_ids.to("cuda")
         outputs = model.generate(input_ids, do_sample=True, top_k = 10, max_new_tokens=50)
         decoded_output = tokenizer.decode(outputs[0][input_ids[0].shape[0]:]).strip()
@@ -114,8 +110,10 @@ def debug_query_inference(model : object, tokenizer : object, queries : dict) ->
     return res_labels 
 
 def calculate_metrics(pred_labels : dict, gold_labels : dict) -> dict:
+    print(f'{gold_labels=}{pred_labels=}')
     res_labels = [[],[]]
     for q_id in pred_labels:
+        print(f'{gold_labels[q_id]["gold_label"]=}{pred_labels[q_id]=}')
         res_labels[0].append(gold_labels[q_id]["gold_label"])
         res_labels[1].append(pred_labels[q_id])
 
@@ -129,7 +127,7 @@ def output_task_results(output_dir : str, model_name : str, used_set : str, resu
     with safe_open_w(f'{output_dir}task_output/{model_name.split("/")[-1]}_0-shot_{used_set}-set.json') as output_file:
         output_file.write(json.dumps(results, ensure_ascii=False, indent=4))
 
-def output_full_metrics(args : dict, full_prompt : str, used_set : str, metrics : dict):
+def output_full_metrics(args : dict, prompt_id : str, full_prompt : str, used_set : str, metrics : dict):
 
     timestamp = datetime.now().strftime("%Y-%m-%d_%H-%M")
 
@@ -139,21 +137,21 @@ def output_full_metrics(args : dict, full_prompt : str, used_set : str, metrics 
     results["prompt"] = full_prompt
     results["set"] = used_set
     results["metrics"] = metrics
-    results["formated_metrics"] =f'| {args.model_optimize_name.split("/")[-1]}_(aplt5)_(gen)_   | {metrics["f1"]} | {metrics["precision"]} | {metrics["recall"]} | - |'
+    results["formated_metrics"] =f'| {args.model.split("/")[-1]}-{prompt_id}   | {metrics["f1"]} | {metrics["precision"]} | {metrics["recall"]} | - |'
 
-    with safe_open_w(f'{args.output_dir}ea_outputs/args_output/{timestamp}_{args.model_optimize_name.split("/")[-1]}_{used_set}-set.json') as output_file:
+    with safe_open_w(f'{args.output_dir}combination_output/{timestamp}_{args.model.split("/")[-1]}_{used_set}-set.json') as output_file:
         output_file.write(json.dumps(results, ensure_ascii=False, indent=4))
 
-def full_evaluate_prompt(model: object, tokenizer: object, queries: dict, qrels: dict, prompts: dict, args : object, used_set : str) -> dict:
+def full_evaluate_prompt(model: object, tokenizer: object, queries: dict, qrels: dict, prompt_id : str, prompt: str, args : object, used_set : str) -> dict:
     # Replace prompt with query info
-    queries_dict = create_qid_prompt_label_dict(queries, qrels, prompts)
+    queries_dict = create_qid_prompt_label_dict(queries, qrels, prompt)
 
     # 0-shot inference from queries TODO
     pred_labels = query_inference(model, tokenizer, queries_dict)
 
     # Compute metrics
     metrics = calculate_metrics(pred_labels, queries_dict)
-    output_full_metrics(args, prompts, used_set, metrics)
+    output_full_metrics(args, prompt_id, prompt, used_set, metrics)
 
     return metrics
 
@@ -162,7 +160,7 @@ def generate_pos_prompts(mistral_prompts : dict):
 
     for task_id, task in mistral_prompts["task_descriptions"].items():
         for ctr_id, ctr in mistral_prompts["ctr_descriptions"].items():
-            for statement_id, statement in mistral_prompts["statement_description"].items():
+            for statement_id, statement in mistral_prompts["statement_descriptions"].items():
                 for option_id, option in mistral_prompts["option_descriptions"].items():
                     combination = mistral_prompts["task_template_prompt_comparison"].replace("$task_description", task).replace("$ctr_description", ctr).replace("$statement_description", statement).replace("$option_description", option)
 
@@ -171,7 +169,7 @@ def generate_pos_prompts(mistral_prompts : dict):
     with safe_open_w(f'prompts/MistralPromptsCombination.json') as output_file:
         output_file.write(json.dumps(prompt_combinations, ensure_ascii=False, indent=4))
 
-    quit()
+    return prompt_combinations
 
 
 def main():
@@ -196,11 +194,11 @@ def main():
 
     args = parser.parse_args()
 
-    model = LlamaForCausalLM.from_pretrained(args.model_optimize_name, device_map="auto")
-    model.quantize_config = GPTQConfig(bits=4, exllama_config={"version":2}, desc_act=True)
+    model = AutoModelForCausalLM.from_pretrained(args.model, device_map="auto")
+    #model.quantize_config = GPTQConfig(bits=4, exllama_config={"version":2}, desc_act=True)
     #model = exllama_set_max_input_length(model, 4096)
 
-    tokenizer = AutoTokenizer.from_pretrained(args.model_optimize_name)
+    tokenizer = AutoTokenizer.from_pretrained(args.model)
 
     # Load dataset, queries, qrels and prompts
     queries = json.load(open(args.queries))
@@ -208,6 +206,9 @@ def main():
     prompts = json.load(open(args.prompts))
 
     combination_prompts = generate_pos_prompts(prompts)
+
+    for prompt_id, prompt in combination_prompts["combination_prompts"].items():
+        full_evaluate_prompt(model, tokenizer, queries, qrels, prompt_id, prompt, args, used_set)
     
 
 if __name__ == '__main__':
