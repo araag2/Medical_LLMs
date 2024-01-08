2aa102a3c7be3882e3c191e3975955fd2779d480
araag2 <artur.guimas@gmail.com>
2024-01-08 13:32:33 +0000

    updated stats and parsers

---
+++ tasks/SemEval2023/cpu_only/README.md
@@ -0,0 +1,23 @@
+## SemEval2023
+
+### Set Compostions 
+
+All sets are balanced in labels, having 50% of Entailment and Contradiction
+
+| **Metrics**    | #Samples | Single | Comparison |
+|:-------------- |:--:|:--:|:--:|
+| Train | 1700 | 1035 | 665 |
+| Dev   | 200  | 140  | 60  | 
+| Test  | 500  | 229  | 271 |
+
+## SemEval2024
+
+All sets are balanced in labels, having 50% of Entailment and Contradiction.
+
+The Dev and Train sets are the same as 2023. Pratice-test includes dev-set
+
+| **Metrics**    | #Samples | Single | Comparison |
+|:-------------- |:--:|:--:|:--:|
+| Train | 1700 | 1035 | 665 |
+| Dev   | 200  | 140  | 60  | 
+| Pratice-Test  | 2142  | 1526  | 616 |
--- tasks/SemEval2023/cpu_only/parser_qrels2queries.py
+++ tasks/SemEval2023/cpu_only/parser_qrels2queries.py
@@ -18,93 +18,28 @@ def safe_open_w(path: str):
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('--input', type=str, help='path to input dir/file', required=True)
-    parser.add_argument('--output', type=str, help='path to output dir/file', required=True)
-    parser.add_argument('--type', type=str, help='type: trials, queries or qrels', required=True, default='trials')
-    args = parser.parse_args()
+    parser.add_argument('--input', type=str, help='path to input dir/file', default='../qrels/qrles2024_pratice-test.json')
+    parser.add_argument('--output', type=str, help='path to output dir/file', default='../queries/queries2024_pratice-test.json')
+    parser.add_argument('--corpus', type=str, help='path to CT Corpus', default='../CT_json/SemEval_CT-corpus.json')
+    args = parser.parse_args() 
 
-    if args.type not in ["trials", "queries", "qrels"]:
-        print('Argument value should be "trials" or "queries" or "qrels"')
-        exit()
+    qrels = json.load(open(args.input))
+    corpus = json.load(open(args.corpus, encoding="utf8"))
 
-    section_substitution = {"Intervention" : "interventions",
-                            "Eligibility" : "eligibity criteria",
-                            "Adverse Events" : "adverse events",
-                            "Results" : "results"}
-    #relevant_qrels_info = ALL      
+    output_dict = {}
 
-    with safe_open_w(args.output) as output_file:
-        output_dict = {}
-
-        # Handling of CT (Clinical Trials) files
-        if args.type == 'trials':
-            for root, dirs, files in os.walk(args.input):
-                files.remove(".DS_Store")
-                CT_corpus = {}
-                for file in tqdm(files):
-                    with open(os.path.join(os.getcwd(), root, file), 'r') as current_CT:
-                        json_CT = json.load(current_CT)
-                        
-                        CT_corpus[json_CT["Clinical Trial ID"]] = {}
-                        for data_field in json_CT:
-                            if data_field != "Clinical Trial ID": 
-                                CT_corpus[json_CT["Clinical Trial ID"]][data_field] = json_CT[data_field]
-
-                output_dict = CT_Corpus
-
-        # Handling of query files
-        elif args.type == 'queries':
-            output_queries = {}
-            with open(args.input) as input_queries_f:
-                json_input_queries = json.load(input_queries_f)
-
-                for query in tqdm(json_input_queries):  
-                    output_queries[query] = {}
-                    for data_field in ['Type', 'Primary_id', 'Secondary_id', 'Section_id']:
-                        if data_field in json_input_queries[query]:
-                            output_queries[query][data_field] = json_input_queries[query][data_field]
-                   
-                    #Uncapitalize Statement and remove "."
-                    sentence = json_input_queries[query]['Statement']
-                    sentence = f'{sentence[0].lower()}{sentence[1:]}'
-                    sentence = sentence if sentence[-1] != "." else sentence[:-1].rstrip()
-                    output_queries[query]['Statement'] = sentence
-
-            output_dict = output_queries
-
-            #Generates extended queries, that include origin CT segments, in order to create actual prompts
-            with safe_open_w(f'{args.output[:-5]}_extended.json') as output_file_extended:
-                extended_query_dict = copy.deepcopy(output_queries)
-                with open(f'{os.getcwd()}/data_json/SemEval2023/CT_corpus.json') as JSON_Corpus:
-                    corpus = json.load(JSON_Corpus)
+    #TO:DO 
+    for query in tqdm(qrels):  
+        output_dict[query] = {}
+        for data_field in ['Type', 'Section_id', 'Primary_id', 'Secondary_id', 'Section_id', 'Statement']:
+            if data_field in qrels[query]:
+               output_dict[query][data_field] = qrels[query][data_field]
                     
-                    for query in extended_query_dict:
-                        extended_query_dict[query]['Primary_id_txt_list'] = corpus[extended_query_dict[query]['Primary_id']][extended_query_dict[query]['Section_id']]
-                        extended_query_dict[query]['Primary_id_txt'] = "\n".join(line for line in extended_query_dict[query]['Primary_id_txt_list'])
-                        if 'Secondary_id' in extended_query_dict[query]:
-                            extended_query_dict[query]['Secondary_id_txt_list'] = corpus[extended_query_dict[query]['Secondary_id']][extended_query_dict[query]['Section_id']]
-                            extended_query_dict[query]['Secondary_id_txt'] = "\n".join(line for line in extended_query_dict[query]['Secondary_id_txt_list'])
+        output_dict[query]['Primary_id_txt_list'] = corpus[output_dict[query]['Primary_id']][output_dict[query]['Section_id']]
+        output_dict[query]['Primary_id_txt'] = "\n".join(line for line in output_dict[query]['Primary_id_txt_list'])
+        if 'Secondary_id' in output_dict[query]:
+            output_dict[query]['Secondary_id_txt_list'] = corpus[output_dict[query]['Secondary_id']][output_dict[query]['Section_id']]
+            output_dict[query]['Secondary_id_txt'] = "\n".join(line for line in output_dict[query]['Secondary_id_txt_list'])
 
-                for query in extended_query_dict:    
-                    # Change section to natural language
-                    output_queries[query]['Section_id'] = section_substitution[json_input_queries[query]['Section_id']]
-                    extended_query_dict[query]['Section_id'] = section_substitution[json_input_queries[query]['Section_id']]
-                output_file_extended.write(json.dumps(extended_query_dict, ensure_ascii=False, indent=4))
-
-
-        # Handling of qrels files
-        elif args.type == 'qrels':
-            output_qrels = {}
-            with open(args.input) as input_qrels_f:
-                json_input_qrels = json.load(input_qrels_f)
-
-                for qrel in tqdm(json_input_qrels):
-                    output_qrels[qrel] = {}
-                    for data_field in json_input_qrels[qrel]:
-                        output_qrels[qrel][data_field] = json_input_qrels[qrel][data_field]
-
-            output_dict = output_qrels
-
-        # Output to file
-        output_file.write(json.dumps(output_dict, ensure_ascii=False, indent=4))
-                    
+    with safe_open_w(args.output) as f:
+        json.dump(output_dict, f, indent=4)
